5b5461fd6caefbda1e322e52023a9a9ba6fd366f
Eric Zhou <Eric.Zhou@ldt-4742405.gfdl.noaa.gov>
2025-07-09 13:51:55 -0400

    test using psql 4

--- src/epmt/epmt_default_settings.py
+++ src/epmt/epmt_default_settings.py
@@ -78,7 +78,7 @@ orm = 'sqlalchemy'
 #db_params = { 'url': 'sqlite:///:memory:', 'echo': False }
 #db_params = {'url': 'postgresql://epmt:Goal2020@workflow1:5432/epmt', 'echo': False}
 
-db_params = {'url': 'postgresql://testuser:testpassword@localhost:5432/testdb', 'echo': False}
+db_params = {'url': 'postgresql://testuser:testpassword@postgres:5432/testdb', 'echo': False}
 
 bulk_insert = True
 

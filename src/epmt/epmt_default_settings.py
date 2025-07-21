8424baf4bf30b26cb7728fec7ceaa5468a77bbf1
Eric Zhou <Eric.Zhou@lrdpub06-p.gfdl.noaa.gov>
2025-07-21 10:29:17 -0400

    retesting 3

--- src/epmt/epmt_default_settings.py
+++ src/epmt/epmt_default_settings.py
@@ -75,9 +75,9 @@ retire_jobs_per_delete_max = 20 # specify the chunk-size to delete jobs in
 # we expect the settings below to be overriden in settings.py
 # depending on the template of your choice
 orm = 'sqlalchemy'
-#db_params = { 'url': 'sqlite:///:memory:', 'echo': False }
+db_params = { 'url': 'sqlite:///:memory:', 'echo': False }
 #db_params = {'url': 'postgresql://epmt:Goal2020@workflow1:5432/epmt', 'echo': False}
-db_params = {'url': 'postgresql://testuser:testpassword@postgres:5432/testdb', 'echo': False}
+#db_params = {'url': 'postgresql://testuser:testpassword@postgres:5432/testdb', 'echo': False}
 
 bulk_insert = True
 
